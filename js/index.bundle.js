06ccd788b95c7a3deb0b895f6ee650108c560437
Вильгельм <vito.fincher@list.ru>
2024-01-28 22:37:00 +1000

    first commit

---
+++ js/index.bundle.js
@@ -0,0 +1,1 @@
+!function(){const e=document.querySelector("#open-mobile-nav-btn"),n=document.querySelector("#close-mobile-nav-btn"),o=document.querySelector(".mobile-nav-wrapper");e.onclick=function(){o.classList.add("mobile-nav-wrapper--open")},n.onclick=function(){o.classList.remove("mobile-nav-wrapper--open")}}();
